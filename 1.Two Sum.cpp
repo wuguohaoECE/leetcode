06bdd2d468158b1d88b0a1d530971a2da894c4eb
solusion <59156859+wuguohaoECE@users.noreply.github.com>
2020-01-31 22:21:26 +0800

    Create 1.Two Sum.cpp

---
+++ 1.Two Sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> map;
+        size_t size = nums.size();
+        int i = 0;
+        while(i < size && !map.count(nums[i])){
+            map[target - nums[i]] = i;
+            ++i;
+        }
+        if(i == size) {
+            return vector<int>();
+        }
+        return vector<int>({map[i], i});
+    }
+};
