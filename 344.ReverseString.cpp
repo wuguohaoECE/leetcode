b674398b78eb86d3061ce360798bc531fd8c5474
solusion <59156859+wuguohaoECE@users.noreply.github.com>
2020-02-03 00:11:14 +0800

    Create 344.ReverseString.cpp

---
+++ 344.ReverseString.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int L = 0, R = s.size() - 1;
+        while(L < R){
+            swap(s[L], s[R]);
+            ++L; --R;
+        }
+    }
+};
